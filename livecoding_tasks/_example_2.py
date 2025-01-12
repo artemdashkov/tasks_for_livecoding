4227ff127ec8f3163d0198701ee836bb491fd08a
last_name <artemdashkov@gmail.com>
2025-01-12 17:55:59 +0300

    add permision

--- livecoding_tasks/_example_2.py
+++ livecoding_tasks/_example_2.py
@@ -1,5 +1,5 @@
 from selenium.webdriver.common.by import By
 def funct_1(a,b):
-    return a+b
+  return a+b
 asdasdfasdrae = 1
 RISK_MANAGEMENT_TOOLS_BLOCK_LOCATOR = (By.CSS_SELECTOR, "[data-type='banner_in_body_block'] [class='grid_grid__2D3md grid_gMdLg__9Xp_H'] >.grid_grid__2D3md:nth-child(6)")
