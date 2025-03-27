c8a14270ad746e0ae4b8aaee30ef3ed7b172c3c2
last_name <artemdashkov@gmail.com>
2025-03-27 23:42:08 +0300

    add new info

--- example_2.py
+++
@@ -1,3 +0,0 @@
-new_file = open("any_file.txt", "a")
-new_file.write("\nSecond string")
-new_file.close()
--- livecoding_tasks/23.find_common_symbol.py
+++ livecoding_tasks/23.find_common_symbol.py
@@ -7,10 +7,5 @@ list_2 = [5,6,7,8,9]
 set_1 = set(list_1)
 set_2 = set(list_2)
 
-print(list(set_1.intersection(set_2)))
-
-# way 2
-for x in list_1:
-    for y in list_2:
-        if x == y:
-            print(x)
+intersection = set_1 & set_2
+print(intersection)
