0f1e9549644252924a9b1bbdd538f62a0d8132b2
last_name <artemdashkov@gmail.com>
2025-03-31 23:01:59 +0300

    add new info

---
+++ livecoding_tasks/25.is_anagram.py
@@ -0,0 +1,11 @@
+# 25. Преобразование списка в строку
+
+list_1 = ['1', '2', '3']
+
+new_string = ""
+
+for x in list_1:
+    new_string += x
+
+print(new_string)
+
---
+++ livecoding_tasks/26.string_is_number.py
@@ -0,0 +1,10 @@
+# Проверка, является ли строка числом
+
+string_1 = "la"
+string_2 = 5
+string_3 = 5.1
+string_4 = ["1", "list"]
+string_5 = ('a', 5)
+string_6 = {"string_6": 5}
+
+
